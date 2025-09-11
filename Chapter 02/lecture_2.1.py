58f9dc0c6044dc0a06e8bff2562130e9f60f4846
Mayank Agarwal <mayankagarwal4348@gmail.com>
2025-09-11 10:06:35 +0530

    Add files via upload

---
+++ Chapter 02/lecture_2.1.py
@@ -0,0 +1,21 @@
+# Topic: Variables; A programer's perspective
+
+ram_bank_balance = 100000
+# ram's bank balance, note that this is +ve
+ram_loan_amount = 500000
+# ram's loan, this is to be returned by him and hence will count as negative
+
+lakshman_bank_balance = 2000000
+# lakshman's bank balance, note that this is +ve
+lakshman_loan_amount = 1000000
+# lakshman's loan, this is to be returned by him and hence will count as negative
+
+net_income = ram_bank_balance + lakshman_bank_balance 
+# net_income is total bank balance of two brothers
+net_liability = ram_loan_amount + lakshman_loan_amount
+# net_liability is the total loan of the brothers
+
+final_amount = net_income - net_liability
+# final_amount is the family's final money (it may be +ve or -ve)
+
+print("So, the family has", final_amount)
---
+++ Chapter 02/lecture_2.2.py
@@ -0,0 +1,13 @@
+# Topic: Variables Revisited Dynamic typing
+
+a = 10
+print(a, type(a))
+
+a= "india"
+print(a, type(a))
+
+n = 10
+print(n, type(n))
+
+n = n/2
+print(n, type(n))
