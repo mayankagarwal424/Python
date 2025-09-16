e47dee82d32e3aa83e7e1b25c48c35de7470a23e
Mayank Agarwal <mayankagarwal4348@gmail.com>
2025-09-16 17:25:14 +0530

    Add files via upload

---
+++ Chapter 03/lecture_3.6.py
@@ -0,0 +1,7 @@
+# Topic: for loop for multiplication tables
+
+n = int(input("Enter a number: "))
+
+for i in range(1,11):
+    tab = i * n
+    print(f"{n} X {i} = {tab}")
---
+++ Chapter 03/lecture_3.7.py
@@ -0,0 +1,18 @@
+# Topic: More on range and for loop without range 
+
+'''
+range(Start, end, step)
+'''
+
+for i in range(1, 11, 2):
+    print(i)
+
+for x in range(9, -1, -1):
+    print(x)
+
+# for without range "forEach"
+
+country = "INDIA"
+
+for letter in country:
+    print(letter)
