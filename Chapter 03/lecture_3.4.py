8e16eb33c4c86fa8eabfae211686883a12bf5aa8
Mayank Agarwal <mayankagarwal4348@gmail.com>
2025-09-16 17:05:53 +0530

    Add files via upload

---
+++ Chapter 03/lecture_3.4.py
@@ -0,0 +1,14 @@
+# Topic: Introduction to for loop
+
+for i in range(10):
+    print(i, "Hello India")
+    print("**************")
+    print("##############")
+
+n = int(input("Enter a number: "))
+
+for i in range(n):
+    if (i%2 == 0):
+        print(i, "Hello India")
+    else:
+        print(i, "Hii World")
---
+++ Chapter 03/lecture_3.5.py
@@ -0,0 +1,9 @@
+# Topic: for loop to add the first n numbers
+
+num = int(input("Enter a number: "))
+sum = 0
+
+for i in range(num):
+    sum += i
+
+print("The answer is: ", sum) 
