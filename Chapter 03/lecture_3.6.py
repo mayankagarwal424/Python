6694737df19ecaa4146a1ce1ecc357b88b5dc410
Mayank Agarwal <mayankagarwal4348@gmail.com>
2025-09-17 23:18:40 +0530

    lecture_3.6.py

--- Chapter 03/lecture_3.6.py
+++ Chapter 03/lecture_3.6.py
@@ -1,7 +1,8 @@
-# Topic: for loop for multiplication tables
+'''Topic: for loop for multiplication tables'''
 
 n = int(input("Enter a number: "))
 
 for i in range(1,11):
     tab = i * n
+
     print(f"{n} X {i} = {tab}")
