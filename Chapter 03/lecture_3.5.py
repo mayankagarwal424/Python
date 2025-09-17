cab625e5889855981a846aeb9cf054bc754043f0
Mayank Agarwal <mayankagarwal4348@gmail.com>
2025-09-17 23:18:07 +0530

    lecture_3.5.py

--- Chapter 03/lecture_3.5.py
+++ Chapter 03/lecture_3.5.py
@@ -1,9 +1,10 @@
-# Topic: for loop to add the first n numbers
+'''Topic: for loop to add the first n numbers'''
 
 num = int(input("Enter a number: "))
 sum = 0
 
 for i in range(num):
     sum += i
+
 
 print("The answer is: ", sum) 
