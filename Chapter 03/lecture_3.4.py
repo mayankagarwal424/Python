b51614c1b26e47929d6c35bf412c51e31d0277c6
Mayank Agarwal <mayankagarwal4348@gmail.com>
2025-09-17 23:16:25 +0530

    lecture_3.4.py

--- Chapter 03/lecture_3.4.py
+++ Chapter 03/lecture_3.4.py
@@ -1,4 +1,4 @@
-# Topic: Introduction to for loop
+'''Topic: Introduction to for loop'''
 
 for i in range(10):
     print(i, "Hello India")
@@ -11,4 +11,5 @@ for i in range(n):
     if (i%2 == 0):
         print(i, "Hello India")
     else:
+
         print(i, "Hii World")
