59bab2a9ea3cfc5dfbc11be5b1f30e2f9c0ededa
Mayank Agarwal <mayankagarwal4348@gmail.com>
2025-09-23 14:09:40 +0530

    lecture_1.2.py

--- Chapter 01/lecture_1.2.py
+++ Chapter 01/lecture_1.2.py
@@ -1,8 +1,9 @@
-# Topic: Printand common mistakes
+'''Topic: Printand common mistakes'''
 
 # we can also display multiple statement using one print function by separating them by ','
 print("Hello INDIA!", "I am Mayank,", "welcome Python Student")
 
 # We can also display numbers using print
 print(10)
+
 print(8.54)
