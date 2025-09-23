68967a2f9911d95fa85d992ceb19131eaf5ef057
Mayank Agarwal <mayankagarwal4348@gmail.com>
2025-09-23 14:12:04 +0530

    lecture_1.5.py

--- Chapter 01/lecture_1.5.py
+++ Chapter 01/lecture_1.5.py
@@ -1,10 +1,13 @@
-# Topic: Variable and Literals
+'''Topic: Variable and Literals'''
 
 #  1st program
 n = (input("Enter your name: "))
 p = (input("type your location: "))
+
 print("Hello", n, "how is the weather in", p, "?")
+
 age = int(input("What is your age: "))
+
 print("Good to know that you are", age, "year old")
 
 # 2nd program
@@ -21,4 +24,5 @@ r = int(input("Enter the radous of Circle: "))
 area = 3.14*r**2
 print("The area of the circle with radius", r, "is", area)
 # here r is "variable" because it may be change..
+
 # but 3.14 is "Literal" because it will not change...
