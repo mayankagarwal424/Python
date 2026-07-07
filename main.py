3d46a87cdb1b582d0eb91b4d9a7994902b5a79bf
Mayank Agarwal <mayankagarwal4348@gmail.com>
2026-07-07 16:48:51 +0530

    Add main.py with greeting and variable examples

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Namastey youtube We are learning python")
+
+## Variable 
+sher = "Mayank Agarwal"
+
+SheriansSchool = "student"      #pascal case
+sheryiansSchool = "Students"    #camel case
+sheryians_school = "students"    #snake case
+
