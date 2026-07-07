85a118db324cbceaf4139d43ebe1cd18169caa30
Mayank Agarwal <mayankagarwal4348@gmail.com>
2026-07-07 23:18:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,3 +37,62 @@
 # print(f"Hello {name}, Happy to know you are {age} year old")
 
 '''Opreation'''
+'''Arithmetic opretors'''
+# a = 4
+# b = 32
+
+# print(a + b)          #provide addition
+# print(a - b)          #provide substraction
+# print(a * b)          #provide multiplication
+# print(b / a)          #provide divition
+# print(b // a)         #provide diviser part after devision
+# print(a ** b)         #provide a ki power b
+# print(b % a)          #provide remider part after division
+
+# print(12 + 4 / 2)     #python is also follow 'BODMAS' rule
+
+'''Assignment opreator'''
+
+# a = 23                #23 is assign to a
+
+'''Compound assignment opreation'''
+
+#a = 20
+
+# a += 20
+# a += 25 
+# a += 30      
+
+# print(a)               #It can also written as like that
+# a -=
+# a *=
+# a /=
+# a **= 
+
+'''Comparison opreator'''
+
+# a = 13.5
+# b = 13
+
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(45 <= 67)
+# print(23 >= 43)
+
+# print(ord("A"))
+# print(ord("B"))
+
+# print("A" > "B")					#provide False because A = 65 and B = 66
+# print("ABC" < "AND")
+
+# print("A" > 34) 					#we can not do this only string to string comparision is allowed
+
+'''Logical opreator'''
+
+# print(12 > 20 and 125 > 110 and 34 ==34 and 46 < 93)				#if all condition is true then only provide "True" answer will be "False"
+
+# print(12 != 12 or 23 == 49 or 10 > 5) 			#if one condition is true provide "True" answer will be "True"
+
+# print(not 12 == 12)					#not reverse the condition answer will be "False"
