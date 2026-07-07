a2c4e9aa73f7df0b1f2639e77fa5ae65237a2f7d
Mayank Agarwal <mayankagarwal4348@gmail.com>
2026-07-07 18:25:58 +0530

    Update main.py
    Comment out existing code examples and explanations.

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-print("Namastey youtube We are learning python")
+# print("Namastey youtube We are learning python")
 
-## Variable 
-sher = "Mayank Agarwal"
+'''Variable'''
+# sher = "Mayank Agarwal"
 
-SheriansSchool = "student"      #pascal case
-sheryiansSchool = "Students"    #camel case
-sheryians_school = "students"    #snake case
+# SheriansSchool = "student"      #pascal case
+# sheryiansSchool = "Students"    #camel case
+# sheryians_school = "students"    #snake case
 
+'''Data types'''
+# a = -34          #integer
+# b = 45.3         #float
+# c = 15/5         #float
+
+# v = 34j          #j is using for imaginary value only j is use for imaginary value other letter gives error
+
+# st = "1234567890 dasdrdnjd !@#$%^&*"        #string
+# print(type(st))
+
+# b = True                       #boolean
+# f = False                      #boolean have either True value or False value (make sure "T" & "F" should be capital)
+# print(type(b))
+
+'''Strings'''
+'''string Sliceing syntax  st[Start: Stop: Step]'''
+
+# a = "Sher coders"
+
+# print(a[5::1])                   #print "coder"
+# print(a[:4:1])                   #print "Sher"
+# print(a[::])                     #print complete string
+
+'''Format printing'''
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: ")
+
+# print(f"Hello {name}, Happy to know you are {age} year old")
+
+'''Opreation'''
